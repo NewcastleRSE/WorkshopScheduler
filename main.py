87655ecc703671a39aacb757429eef19348fa057
dkwdaft <31158132+dkwdaft@users.noreply.github.com>
2022-03-18 11:57:44 +0000

    Updated to conform to pep 8

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from guizero import App , MenuBar
+from guizero import App, MenuBar
+
+
 def import_csv():
     print("import csv button pressed")
+
 
 app = App(title="WorkshopScheduler")
 menubar = MenuBar(app,
                   toplevel=["File"],
                   options=[
-                      [["Import csv ",import_csv()]]
+                      [["Import csv ", import_csv()]]
                   ])
 app.display()
+ 
