538d129c6ad279300781a2596342a1ca4ba271fb
dkwdaft <31158132+dkwdaft@users.noreply.github.com>
2022-04-01 14:32:07 +0100

    add new python file to port existing work to tkinter

---
+++ ui-mockup-test-tkinter.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+
+def import_csv():
+    print("import csv button pressed")
+
+
+def export_to_html():
+    print("export to HTML pressed ")
+
+def export_to_csv():
+    print("export to csv pressed ")
+root = Tk()
+menubar = Menu(root)
+file_menu = Menu(menubar, tearoff=0)
+file_menu.add_command(label=,command=)
+file_menu.add_command(label=,command=)
+file_menu.add_command(label=,command=)
+root.mainloop()
