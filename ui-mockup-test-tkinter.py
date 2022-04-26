db780758b6a0dd3ea1e7781536101badfe5b3924
dkwdaft <31158132+dkwdaft@users.noreply.github.com>
2022-04-26 14:16:54 +0100

    completed layout and changed start_time_input_box  width to match expected value

--- ui-mockup-test-tkinter.py
+++ ui-mockup-test-tkinter.py
@@ -30,20 +30,23 @@ filemenu.add_command(label="Exit", command=root.quit)
 menubar.add_cascade(label="File", menu=filemenu)
 
 # StartTime Label and input field
-start_time_label = Label(root, text="start time:", justify=LEFT).grid(row=1,column=1)
-start_time_input_box = Entry(root,justify=LEFT).grid(row=1,column=2)
+start_time_label = Label(root, text="start time:", justify=LEFT)
+start_time_label.grid(row=1, column=1
+start_time_input_box = Entry(root, justify=LEFT, width=5)
+start_time_input_box.grid(row=1, column=2, sticky=W)
 
 # Frame
 frame = Frame(root)
-frame.grid(row=3, column=3)
+frame.grid(row=3, column=2, sticky=W)
 # pandastable Initialisation
 pt = Table(frame)
 # Import CSV template file
 pt.importCSV("schedule-template/Template-schedule-blank.csv")
-#show table
+# show table
 pt.show()
 #
 root.config(menu=menubar)
 # add window bar title
 root.title("WorkshopScheduler")
+
 root.mainloop()
